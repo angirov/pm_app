ac69512da13aa865e0cf9815ab542ab4c48e2c87
Vladimir Angirov <angirov@posteo.net>
2023-03-17 11:25:22 +0100

    author's name unique + test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, unique=True, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True),
                         server_default=func.now())
 
--- dummy.py
+++ dummy.py
@@ -1,11 +1,24 @@
 from app import app, db, Author, Paper
+from sqlalchemy.exc import IntegrityError
 
 app.app_context().push()
 
-a = Author(name="Vova")
-db.session.add(a)
+a = Author(name="Pupu2")
 
-p = Paper(text="hello world", author=a.id)
+def db_add_object(obj):
+    try:
+        db.session.add(obj)
+        db.session.commit()
+    except IntegrityError as e:
+        db.session.rollback()
+        print(e)
+
+db_add_object(a)
+
+a = Author.query.filter_by(name="Pupu2").first()
+
+p = Paper(text="hello world!!22222", author_id=a.id)
 db.session.add(p)
-
 db.session.commit()
+
+print(">>>>>>>>>> done!")
